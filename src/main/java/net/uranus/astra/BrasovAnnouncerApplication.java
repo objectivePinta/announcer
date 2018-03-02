0a0f18122901bc48ef122790d95377fff584eb35
Andrei Pinta <andrei.pinta@metrosystems.net>
2018-03-02 20:33:37 +0200

    add cors

--- src/main/java/net/uranus/astra/BrasovAnnouncerApplication.java
+++ src/main/java/net/uranus/astra/BrasovAnnouncerApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BrasovAnnouncerApplication {
@@ -29,6 +32,16 @@ public class BrasovAnnouncerApplication {
 
     };
 
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurerAdapter() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000","https://wow-ui.herokuapp.com");
+      }
+    };
   }
 
 }
